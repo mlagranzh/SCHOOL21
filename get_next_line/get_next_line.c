e1c6d122eafbf54445fe50c870a6a4a25b6f60ec
mlagranzh <75257243+mlagranzh@users.noreply.github.com>
2021-05-28 20:03:35 +0300

    2.0

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -25,13 +25,51 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	return (p);
 }
 
-
-
-int error(char *buf, char *ostatok)
+char	*return_ostatok(char *buf, char *ostatok, int rs, char **line)
 {
-	free(buf);
-	free(ostatok);
-	return (-1);
+	char	*tmp;
+
+	if (rs == -2)
+	{
+		*line = ft_substr(ostatok, 0, ft_strchr(ostatok, '\n') - 1);
+		tmp = ft_strdup(ostatok + ft_strchr(ostatok, '\n'));
+		free(ostatok);
+		return (tmp);
+	}
+	if (rs == -1)
+	{
+		ostatok = NULL;
+		return (ostatok);
+	}
+	if (rs > 0)
+	{
+		buf[rs] = '\0';
+		tmp = ft_strdup(ostatok);
+		free(ostatok);
+		ostatok = ft_strjoin(tmp, buf);
+		free(tmp);
+		free(buf);
+		return (ostatok);
+	}
+	return (NULL);
+}
+
+int	return_flag (char *buf, char *ostatok, int rs, char **line)
+{
+	if (rs < 0)
+	{
+		free(buf);
+		free(ostatok);
+		return (-1);
+	}
+	if (rs == 0)
+	{
+		*line = ft_substr(ostatok, 0, ft_strchr(ostatok, '\0'));
+		free(ostatok);
+		free(buf);
+		return (0);
+	}
+	return (0);
 }
 
 int	get_next_line(int fd, char **line)
@@ -39,42 +77,24 @@ int	get_next_line(int fd, char **line)
 	char		*buf;
 	static char	*ostatok;
 	int			rs;
-	char		*tmp;
+	int			flag;
 
 	if (fd < 0 || line == NULL || BUFFER_SIZE < 1)
 		return (-1);
 	if (ostatok == NULL)
 		ostatok = ft_calloc(1, 1);
-  while (1)
-  {
-    if (ft_strchr(ostatok, '\n') != 0)
-    {
-      *line = ft_substr(ostatok, 0, ft_strchr(ostatok, '\n') - 1);
-	  tmp = ft_strdup(ostatok + ft_strchr(ostatok, '\n'));
-	  free(ostatok);
-      ostatok = tmp;
-      return (1);
-    }
-    else
-    {
-      buf = malloc(BUFFER_SIZE + 1);
-      rs = read(fd, buf, BUFFER_SIZE);
-	  if (rs < 0)
-		return(error(buf, ostatok));
-      if (rs == 0)
-      {
-        *line = ft_substr(ostatok, 0, ft_strchr(ostatok, '\0'));
-	  	free(ostatok);
-		free(buf);
-		ostatok = NULL;
-        return (0);
-      }
-      buf[rs] = '\0';
-	  tmp = ft_strdup(ostatok);
-	  free(ostatok);
-      ostatok = ft_strjoin(tmp, buf);
-	  free(tmp);
-	  free(buf);
-    }
-  }
+	while (1)
+	{
+		if (ft_strchr(ostatok, '\n') != 0)
+		{
+			ostatok = return_ostatok(buf, ostatok, -2, line);
+			return (1);
+		}
+		buf = malloc(BUFFER_SIZE + 1);
+		rs = read(fd, buf, BUFFER_SIZE);
+		flag = return_flag(buf, ostatok, rs, line);
+		ostatok = return_ostatok(buf, ostatok, rs, line);
+		if (rs == 0 || rs < 0)
+			return (flag);
+	}
 }
