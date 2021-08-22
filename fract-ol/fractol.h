aadb51928157adf3ea400c0683529d2b93de5023
mlagranzh <75257243+mlagranzh@users.noreply.github.com>
2021-08-22 05:12:14 +0300

    Add files via upload

---
+++ fract-ol/fractol.h
@@ -0,0 +1,63 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "minilibx/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include "Libft/libft.h"
+
+typedef struct s_complex
+{
+	double	re;
+	double	im;
+}			t_complex;
+
+typedef struct s_vars
+{
+	void		*mlx;
+	void		*win;
+	t_complex	min;
+	t_complex	max;
+	t_complex	factor;
+	t_complex	k;
+	t_complex	z;
+	t_complex	c;
+	char		*fract_ol;
+	double		zoom;
+	int			height;
+	int			width;
+	int			x_bias;
+	int			y_bias;
+	int			color;
+}				t_vars;
+
+typedef struct s_image
+{
+	int		color;
+	int		pixel;
+	char	*buffer;
+	void	*image;
+	int		pixel_bits;
+	int		line_bytes;
+	int		endian;
+	int		color1;
+}				t_image;
+
+t_complex	init_complex(double re, double im);
+int			mouse_press_hook(int button, int x, int y, t_vars *vars);
+int			key_press(int keycode, t_vars *vars);
+void		init_vars(t_vars *vars);
+void		update_buffer(t_image *img, int i, int x, int y);
+
+void		draw_julia(t_vars	*vars, t_image	*img);
+void		julia(t_vars *vars);
+int			julia_motion(int x, int y, t_vars *vars);
+
+void		mandelbrot(t_vars *vars);
+void		draw_mandelbrot(t_vars *vars, t_image *img);
+
+void		mandelbar(t_vars *vars);
+void		draw_mandelbar(t_vars *vars, t_image *img);
+
+#endif
---
+++ fract-ol/main.c
@@ -0,0 +1,114 @@
+#include "fractol.h"
+
+int	key_press(int keycode, t_vars *vars)
+{
+	if (keycode == 53)
+		exit(0);
+	if (keycode == 123)
+		vars -> x_bias -= 1;
+	if (keycode == 124)
+		vars -> x_bias += 1;
+	if (keycode == 125)
+		vars -> y_bias += 1;
+	if (keycode == 126)
+		vars -> y_bias -= 1;
+	if (keycode == 18)
+		vars -> color = 5;
+	if (keycode == 19)
+		vars -> color = -10;
+	if (keycode == 20)
+		vars -> color = -100;
+	if (!ft_strncmp(vars -> fract_ol, "mandelbar", 9))
+		mandelbar(vars);
+	if (!ft_strncmp(vars -> fract_ol, "mandelbrot", 10))
+		mandelbrot(vars);
+	if (!ft_strncmp(vars -> fract_ol, "julia", 5))
+		julia(vars);
+	return (0);
+}
+
+t_complex	init_complex(double re, double im)
+{
+	t_complex	complex;
+
+	complex.re = re;
+	complex.im = im;
+	return (complex);
+}
+
+int	mouse_press_hook(int button, int x, int y, t_vars *vars)
+{
+	if (button == 4)
+	{
+		vars -> zoom = -0.1;
+		if (!ft_strncmp(vars -> fract_ol, "mandelbrot", 10))
+			mandelbrot(vars);
+		if (!ft_strncmp(vars -> fract_ol, "julia", 5))
+			julia(vars);
+		if (!ft_strncmp(vars -> fract_ol, "mandelbar", 9))
+			mandelbar(vars);
+	}
+	if (button == 5)
+	{
+		vars -> zoom = 0.1;
+		if (!ft_strncmp(vars -> fract_ol, "mandelbrot", 10))
+			mandelbrot(vars);
+		if (!ft_strncmp(vars -> fract_ol, "julia", 5))
+			julia(vars);
+		if (!ft_strncmp(vars -> fract_ol, "mandelbar", 9))
+			mandelbar(vars);
+	}
+	return (0);
+}
+
+void	init_vars (t_vars *vars)
+{
+	vars -> height = 500;
+	vars -> width = 500;
+	vars -> min = init_complex(-2.0, -2.0);
+	vars -> max.re = 2.0;
+	vars -> max.im = vars -> min.im + (vars -> max.re - vars -> min.re) \
+					* vars -> height / vars -> width;
+	vars -> mlx = mlx_init();
+	vars -> win = mlx_new_window(vars -> mlx, vars -> height, \
+									vars -> width, "FRACT-OL");
+	vars -> zoom = 0;
+	vars -> x_bias = 0;
+	vars -> y_bias = 0;
+}
+
+int	main(int argc, char **argv)
+{
+	t_vars	vars;
+
+	if (argc != 2)
+	{
+		printf("Укажи название фрактала: mandelbrot/julia/mandelbar\n");
+		exit (1);
+	}
+	init_vars(&vars);
+	if (!ft_strncmp(argv[1], "mandelbrot", 10))
+	{
+		vars.fract_ol = "mandelbrot";
+		mandelbrot(&vars);
+	}
+	else if (!ft_strncmp(argv[1], "julia", 5))
+	{
+		vars.fract_ol = "julia";
+		mlx_hook(vars.win, 6, 0, julia_motion, &vars);
+	}
+	else if (!ft_strncmp(argv[1], "mandelbar", 9))
+	{
+		vars.fract_ol = "mandelbar";
+		mandelbar(&vars);
+	}
+	else
+	{
+		printf("Укажи название фрактала: mandelbrot/julia/mandelbar\n");
+		exit (1);
+	}
+	mlx_hook(vars.win, 4, 0, mouse_press_hook, &vars);
+	mlx_hook(vars.win, 2, 0, key_press, &vars);
+	mlx_loop(vars.mlx);
+	return (0);
+}
